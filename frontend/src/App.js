4e8cf9873a5da979892f990b04983b5badfdaaeb
gulyaev <gulyaev.gvs@gmail.com>
2024-12-17 02:39:48 +0500

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+
+class App extends React.Component {
+  state = { 
+    details: [],
+  }
+
+  componentDidMount() {
+    let data;
+    axios.get('http://localhost:8000/api/')
+      .then(res => {
+        data=res.data
+        this.setState(
+          {details: data}
+        )
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  render () {
+    debugger
+    return (
+      <div>
+        <header>
+          <h1>Данные из Django</h1>
+        </header>
+        <br></br>
+        {this.state.details.map((output, id)=>{
+          return <div key={id}>
+            <div>
+              <h2>{output.title}</h2>
+              <p>{output.channel}</p>
+            </div>
+          </div>
+        })}
+      </div>
+    )
+  }
+}
+
+export default App;
