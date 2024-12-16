94b460056f14672a6ded2dc50c434de95c42f855
gulyaev <gulyaev.gvs@gmail.com>
2024-12-17 03:28:01 +0500

    fix: commit

--- djangoreact/djangoreact/settings.py
+++ djangoreact/djangoreact/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -62,6 +62,9 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_ALLOW_ALL = True
 # Разрешить все источники
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://5.159.100.113/',  # ваш фронтенд
+]
 
 ROOT_URLCONF = 'djangoreact.urls'
 
